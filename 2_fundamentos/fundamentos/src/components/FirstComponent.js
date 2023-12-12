5baf43e517c2fee513074194ddb29ce909c8a124
luanacolombo <luuana.colombo@gmail.com>
2023-12-11 22:02:55 -0300

    Comentários nos componentes

--- 2_fundamentos/fundamentos/src/components/FirstComponent.js
+++ 2_fundamentos/fundamentos/src/components/FirstComponent.js
@@ -1,9 +1,15 @@
 const FirstComponent = () => {
+    /*
+    multi line
+    */
+
     return (
         <div>
+            {/* algum comentário*/}
             <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
         </div>
-    )
-}
+    );
+};
 
 export default FirstComponent;
