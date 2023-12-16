9c5d6ac691ee815589a5dd0157dca97eb5380dea
luanacolombo <luuana.colombo@gmail.com>
2023-12-16 19:07:23 -0300

    Imagens em src

--- 3_avancando_em_react/avancando/src/App.js
+++ 3_avancando_em_react/avancando/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+import City from "./assets/city.jpg";
+
 function App() {
   return (
     <div className="App">
@@ -7,6 +9,10 @@ function App() {
       {/*Imagem em public*/}
       <div>
         <img src="img1.jpg" alt="Paisagem" />
+      </div>
+      {/*Imagem em asset*/}
+      <div>
+        <img src={City} alt="Cidade" />
       </div>
     </div>
   );
