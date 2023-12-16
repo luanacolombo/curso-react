7d5171b39a24c247f3b2fb6546efe4a66d857973
luanacolombo <luuana.colombo@gmail.com>
2023-12-16 18:55:29 -0300

    Imagens públicas no React

---
+++ 3_avancando_em_react/avancando/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/*Imagem em public*/}
+      <div>
+        <img src="img1.jpg" alt="Paisagem" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
