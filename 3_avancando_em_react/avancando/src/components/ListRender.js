9267dc0cb006ca1d485a28f12b751159bf6463ef
luanacolombo <luuana.colombo@gmail.com>
2023-12-17 13:23:58 -0300

    Previous state

--- 3_avancando_em_react/avancando/src/components/ListRender.js
+++ 3_avancando_em_react/avancando/src/components/ListRender.js
@@ -3,11 +3,20 @@ import { useState } from 'react'
 const ListRender = () => {
     const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]); //array do tipo lista
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         { id: 1, name: "Matheus", age: 31 },
-        { id: 18164891, name: "João", age: 28 },
-        { id: 51651561, name: "Pedro", age: 44 },
+        { id: 2, name: "João", age: 28 },
+        { id: 3, name: "Pedro", age: 44 },
     ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4) //Math.floor arredonda pra baixo, *3 pois é o n. de itens da lista
+
+        setUsers((prevUsers) => { //deletar, executa uma função, prevUsers resultado anterior dos usuários
+            console.log(prevUsers) //mostra no console
+            return prevUsers.filter((user) => randomNumber !== user.id) //manipulação p/ alterar o state dos usuários, filtra algum usuário que tenha o id diferente dos id passados antes
+        }) //os que derem match ele vai excluir, os que tiverem id diferente ele vai manter na lista
+    }
 
     return (
         <div>
@@ -23,6 +32,7 @@ const ListRender = () => {
                     </li> //{user.name} - {user.age} imprime os dados na tela
                 ))}
             </ul>
+            <button onClick={deleteRandom}>Delete random user</button>
         </div>
     )
 }
