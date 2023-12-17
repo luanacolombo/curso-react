9da13fb5cfcb675a9e2d48d5ed82cdcedf8ed92f
luanacolombo <luuana.colombo@gmail.com>
2023-12-17 14:43:11 -0300

    Introdução a props

--- 3_avancando_em_react/avancando/src/App.js
+++ 3_avancando_em_react/avancando/src/App.js
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
 import City from "./assets/city.jpg";
 import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
+  const name = "Joaquim"
+  const [userName] = useState("Maria")
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -20,6 +25,7 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      <ShowUserName name={userName} /> {/*esse componente tem acesso a uma prop chamada name com o valor da const declarada*/}
     </div>
   );
 }
