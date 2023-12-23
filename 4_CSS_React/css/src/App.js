9e6cc630b0fd749d1b2227bc841acb3df9748679
luanacolombo <luuana.colombo@gmail.com>
2023-12-23 18:41:31 -0300

    CSS inline

--- 4_CSS_React/css/src/App.js
+++ 4_CSS_React/css/src/App.js
@@ -5,10 +5,13 @@ function App() {
   return (
     <div className="App">
       {/*CSS global*/}
-      <h1>Re act com CSS</h1>
+      <h1>React com CSS</h1>
       {/*CSS de componente*/}
       <MyComponent />
-      <p>Este parágrafo é do App.js</p> {/*pegou o css do MyComponent*/}
+      <p>Este parágrafo é do App.js</p> {/*pegou o css do MyComponent */}
+      {/*Inline CSS*/}
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline</p>
     </div>
   );
 }
