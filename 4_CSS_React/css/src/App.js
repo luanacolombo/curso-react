057b4425f352ea864e30e07054e01a97c6404acc
luanacolombo <luuana.colombo@gmail.com>
2023-12-23 18:59:02 -0300

    CSS inline dinâmico

--- 4_CSS_React/css/src/App.js
+++ 4_CSS_React/css/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import { useState } from "react";
 
 function App() {
+  const n = 15
+  const [name] = useState("Matheus")
+
   return (
     <div className="App">
       {/*CSS global*/}
@@ -12,6 +16,10 @@ function App() {
       {/*Inline CSS*/}
       <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
         Este elemento foi estilizado de forma inline</p>
+      {/*CSS Inline dinâmico*/}
+      <h2 style={n < 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS dinâmico</h2> {/*primeiro () se for verdadeiro e segundo se for falso*/}
+      <h2 style={n > 10 ? ({ color: "purple" }) : ({ color: "pink" })}>CSS dinâmico</h2> {/*primeiro () se for verdadeiro e segundo se for falso*/}
+      <h2 style={name === "Matheus" ? ({ color: "green", backgroundColor: "#000" }) : null}>Teste nome</h2> {/*primeiro () se for verdadeiro e segundo se for falso*/}
     </div>
   );
 }
