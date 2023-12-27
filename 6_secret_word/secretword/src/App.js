66f84b1004a2fe123b6eb6b3386a59efd963595c
luanacolombo <luuana.colombo@gmail.com>
2023-12-27 11:13:19 -0300

    Criando componentes do projeto

--- 6_secret_word/secretword/src/App.js
+++ 6_secret_word/secretword/src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+//CSS
 import './App.css';
+//React
+import { useCallback, useEffect, useState } from "react";
+//data
+import { wordsList } from "./data/words";
+//components 
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />} {/*quando o estágio do game for start, irá mostrar o StartScreen*/}
+      {gameStage === 'game' && <Game />} {/*quando o estágio do game for game, irá mostrar o Game*/}
+      {gameStage === 'end' && <GameOver />} {/*quando o estágio do game for end, irá mostrar o GameOver*/}
     </div>
   );
 }
