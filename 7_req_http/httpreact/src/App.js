e488c839b302254a6b06a18557c5698d2355565e
luanacolombo <luuana.colombo@gmail.com>
2024-01-05 20:30:25 -0300

    Adicionando dados com React

--- 7_req_http/httpreact/src/App.js
+++ 7_req_http/httpreact/src/App.js
@@ -5,7 +5,10 @@ import { useState, useEffect } from "react";
 const url = "http://localhost:3000/products"; //url base da API
 
 function App() {
-  const [products, setProducts] = useState([]); //products é a lista de produtos que está no data > db.json
+  const [products, setProducts] = useState([]); //products é a lista de produtos que está no data > db.json, products salva e setProducts ajuda a colocar os produtos em algum lugar
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
 
   //1 - resgatando dados
   useEffect(() => { //chamada assincrona c/ o useEffect
@@ -19,6 +22,25 @@ function App() {
     fetchData();
   }, []);
 
+  //2 - add de produtos
+  const handleSumit = async (e) => { //função que envia o formulário
+    e.preventDefault() //não submete o formulário do jeito tradicional
+
+    const product = { //cria o objeto para envio de todos os dados p/ o backend 
+      name,
+      price
+    }
+
+    const res = await fetch(url, { //requisição
+      //esse segundo obj configura como vai ser a requisição
+      method: "POST",
+      headers: { //cabeçalios da requisição, podemos transmitir na requisição que tipo de conteúdo estamos manipulando
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product), //corpo da requisição 
+    })
+  }
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
@@ -29,6 +51,20 @@ function App() {
           </li>
         ))}
       </ul>
+      {/*onSumit nosso evento que dispara a função handleSumit*/}
+      <div className="add-product">
+        <form onSubmit={handleSumit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)} /> {/*onChange faz a manipulação do dado pegando o evento de digitar e extraindo o valor do input após esse evento*/}
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} /> {/*onChange faz a manipulação do dado pegando o evento de digitar e extraindo o valor do input após esse evento*/}
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
