692094e207c13fe162ec798b278ac4fb3cb7757c
luanacolombo <luuana.colombo@gmail.com>
2024-01-20 19:03:00 -0300

    Configurando o React Router

--- 8_react_router/reactrouter/src/App.js
+++ 8_react_router/reactrouter/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
+//1 - config react router
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//pages
+import Home from './pages/Home'
+import About from './pages/About'
+
 function App() {
   return (
     <div className="App">
-      <h1>React Router</h1>
+      <h1>React Router</h1> {/*elementos fora do Routes, por exemplo, estarão sendo repetidos em todas as páginas*/}
+      <BrowserRouter>
+        <Routes>
+          {/*path="/" home da aplicação, element={<Home />} componente impresso quando o usuário acessar essa rota*/}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
